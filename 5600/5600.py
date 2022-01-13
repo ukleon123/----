6578fdf875be4a059a051dbb6840a5f09d4c4cee
ukleon123 <71366142+ukleon123@users.noreply.github.com>
2022-01-13 12:38:17 +0900

    baekjoon 5600 clear

--- 5600/5600.py
+++ 5600/5600.py
@@ -1,13 +1,42 @@
 import sys
 
 if __name__ == "__main__":
-    source, motor, cable = list(map(int, sys.stdin.readline().split()))
-    test = int(sys.stdin.readline())
+    num = sum(list(map(int, sys.stdin.readline().split())))
+    T = int(sys.stdin.readline())
 
-    broken = []
-    undefined = []
-    certified = []
+    check = {}
+    testCase = []
 
-    for i in range(test):
-        
-        pass
+    for i in range(T):
+        case = list(map(int, sys.stdin.readline().split()))
+        certified = case[3]
+        partNum = case[0:3]
+
+        if certified == 1:
+            for i in partNum:
+                check[i] = 1
+            T -= 1
+        else:
+            for i in partNum:
+                try:
+                    if check[i] != 1:
+                        check[i] = 2
+                except KeyError:
+                    check[i] = 2
+            testCase.append(partNum)
+
+    while testCase:
+        for i in testCase:
+            if check[i[0]] == 1 and check[i[1]] == 1:
+                check[i[2]] = 0
+            elif check[i[1]] == 1 and check[i[2]] == 1:
+                check[i[0]] = 0
+            elif check[i[0]] == 1 and check[i[2]] == 1:
+                check[i[1]] = 0
+        del testCase[-1]
+
+    for i in range(num):
+        try:
+            print(check[i + 1])
+        except KeyError:
+            print(2)
