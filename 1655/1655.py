f8a0e5e46fe210cf6b68f98f55e6a8d080a20518
ukleon123 <71366142+ukleon123@users.noreply.github.com>
2022-01-21 20:14:24 +0900

    1655 clear

--- 1655/1655.py
+++ 1655/1655.py
@@ -1,37 +1,31 @@
 import sys
-def binary_insert(new,  data):
-    
-    tmp = data
-    idx = 0
-    while len(tmp) > 1:
-        if tmp[len(tmp) // 2] == new:
-            idx += (len(tmp) // 2) + 1
-            break
-        elif tmp[len(tmp) // 2] > new:
-            idx += (len(tmp) // 2) + 1
-            tmp = tmp [idx:]
-            pass
-        elif tmp[len(tmp) // 2] < new:
-            idx += (len(tmp) // 2) + 1
-            tmp = tmp [:idx + 1]
-            pass
-    data.insert(idx, new)
-    return data 
-    
+import heapq
+
 if __name__ == "__main__":
-    num = int(sys.stdin.readline())
-    data = list()
-    for i in range(num):
-        flag = 1
-        new = int(sys.stdin.readline())
-        if i < 2:
-            data.append(new)
-            data.sort()
+    front= []
+    rear = []
+    N = int(sys.stdin.readline())
+    
+    for i in range(N):
+        M = int(sys.stdin.readline())
+        
+        heapq.heappush(front, -1 * M)
+        try:
+            if front[0] * -1 > rear[0]:
+                tmp = heapq.heappop(front)
+                heapq.heappush(rear, tmp * -1)
+        except:
+            pass
+
+        if abs(len(front) - len(rear)) > 1:
+            if len(front) > len(rear):
+                tmp = heapq.heappop(front)
+                heapq.heappush(rear, tmp * -1)
+            else:
+                tmp = heapq.heappop(rear)
+                heapq.heappush(front, tmp * -1)
+
+        if len(front) >= len(rear):
+            print(front[0] * -1)
         else:
-            binary_insert(new, data)
-        if (i + 1) % 2:
-            print(data[i // 2])
-        else:
-            print(data[(i + 1) // 2 - 1])
-        flag = 1
-    pass
+            print(rear[0])
