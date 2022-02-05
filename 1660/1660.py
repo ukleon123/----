fa51123116bc87e4559856bbe997f93442766e06
ukleon123 <71366142+ukleon123@users.noreply.github.com>
2022-02-05 17:18:35 +0900

    having trouble in solving 1660

--- 1660/1660.py
+++ 1660/1660.py
@@ -1,4 +1,25 @@
 import sys
 
 if __name__ == "__main__":
-    pass
+    N = int(sys.stdin.readline())
+
+    dp = [0]
+    tetra = [1]
+    adder = 3
+    offset = 3
+
+    idx_dp = 0
+    idx_tetra = 0
+    for i in range(1, N + 1):
+        if i == tetra[idx_tetra]:
+            idx_dp = 0
+            dp.append(dp[idx_dp] + 1)
+            tetra.append(tetra[idx_tetra] + adder)
+            adder  += offset
+            offset += 1
+            idx_dp += 1
+            idx_tetra += 1
+        else:
+            dp.append(dp[idx_dp] + 1)
+            idx_dp += 1
+    print(dp[-1])
